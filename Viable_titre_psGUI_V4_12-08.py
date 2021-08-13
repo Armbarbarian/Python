6bfcd342231c824b2ffe1a044a7ec68d0eb20878
Dan Goodall <daniel_goodall@outlook.com>
2021-08-13 11:57:27 +0100

    VT_psg complete

--- Viable_titre_psGUI_V4_12-08.py
+++ Viable_titre_psGUI_V4_12-08.py
@@ -1,6 +1,7 @@
 # MySimplegui Viable Titre
 
 import PySimpleGUI as sg
+import numpy as np
 import pandas as pd
 
 # sg.theme_previewer()
@@ -12,15 +13,25 @@ EXCEL_FILE = 'out_VT_12-08.xlsx'
 df = pd.read_excel(EXCEL_FILE)
 df
 
+font = ('Calibri', 14)
+font_small = ('Calibri', 12)
+
 # set the layout of the window
 layout1 = [
-    [sg.Text('Calculate the Viable Titre of your Strain:')],
-    [sg.Text('Strain', size=(10, 1)), sg.InputText(key='Strain', size=(10, 1))],
-    [sg.Text('Condition', size=(10, 1)), sg.Combo(['LB', 'Kan', 'Cm', 'Tc', 'Rif'], key='Condition', size=(10, 1))],
-    [sg.Text('Dilution',  size=(10, 1)),
-        sg.Combo(['10e-1', '10e-2', '10e-3', '10e-4', '10e-5', '10e-6', '10e-7', '10e-8'], key='Dilution', size=(10, 1))],
-    [sg.Text('Colonies_A', size=(10, 1)), sg.InputText(key='Colonies_A', size=(10, 1))],
-    [sg.Submit(), sg.Button('Clear'), sg.Exit()]
+    [sg.Text('Calculate the Viable Titre of your Strain:', font=font)],
+    [sg.Text('Strain', size=(10, 1), font=font), sg.InputText(key='Strain', size=(15, 1), font=font)],
+    [sg.Text('Condition', size=(10, 1), font=font), sg.Combo(['LB', 'Kan', 'Cm', 'Tc', 'Rif'], key='Condition', size=(15, 1), font=font)],
+    [sg.Text('Volume plated (mL)', size=(10, 1), font=font), sg.Combo(['0.1', '0.01'], key='Volume', size=(15, 1), font=font)],
+    [sg.Text('Dilution_1',  size=(10, 1), font=font),
+        sg.Combo(['10e-1', '10e-2', '10e-3', '10e-4', '10e-5', '10e-6', '10e-7', '10e-8'], key='Dilution_1', size=(15, 1), font=font)],
+    [sg.Text('Dilution_2',  size=(10, 1), font=font),
+        sg.Combo(['10e-1', '10e-2', '10e-3', '10e-4', '10e-5', '10e-6', '10e-7', '10e-8'], key='Dilution_2', size=(15, 1), font=font)],
+    [sg.Text('Colonies_1', size=(10, 1), font=font), sg.InputText(key='Colonies_1A', size=(15, 1), font=font),
+        sg.InputText(key='Colonies_1B', size=(15, 1), font=font)],
+    [sg.Text('Colonies_2', size=(10, 1), font=font), sg.InputText(key='Colonies_2A', size=(15, 1), font=font),
+        sg.InputText(key='Colonies_2B', size=(15, 1), font=font)],
+    [sg.Text('Viable Titre', size=(10, 1), font=font), sg.InputText('', key='Titre', size=(15, 1), font=font)],
+    [sg.Button('Calculate cells/mL', font=font), sg.Submit('Update Spreadsheet', font=font), sg.Button('Clear', font=font), sg.Exit(font=font)]
 ]
 
 # clear function button
@@ -31,11 +42,12 @@ def clear_input():
         window[key]('')
     return None
 
-# add more boxes and calculations etc....
 
+# set empty results string for VT output
+result = ''
 
 # set up the window and the layout to use for the window
-window = sg.Window('Viable Titre PySimpleGUI', layout1, size=(500, 300))
+window = sg.Window('Viable Titre GUI', layout1, size=(500, 400))
 
 # while loop to keep the window up unless user closes it
 while True:
@@ -44,10 +56,17 @@ while True:
         break
     if event == 'Clear':
         clear_input()
-    if event == 'Submit':
+    if event == 'Calculate cells/mL':
+        average1 = np.mean([int(values['Colonies_1A']), int(values['Colonies_1B'])])
+        average2 = np.mean([int(values['Colonies_2A']), int(values['Colonies_2B'])])
+        vol = float(values['Volume'])
+        dilutions = np.sum([float(values['Dilution_1']), float(values['Dilution_2'])])
+        result = np.sum([average1, average2]) / (vol * dilutions)
+        window['Titre'].update(result)
+    if event == 'Update Spreadsheet':
         #print(event, values)
         df = df.append(values, ignore_index=True)
         df.to_excel(EXCEL_FILE, index=False)
         sg.popup('Data Saved!')
-        clear_input()
+        # clear_input()
 window.close()
