cf44ef2ab4bb63fa6b1639b605e72069850763b9
Dan Goodall <59060337+Armbarbarian@users.noreply.github.com>
2021-08-16 09:44:26 +0100

    VT program

--- Viable Titre/Viable_titre_psGUI_V4_12-08.py
+++ Viable Titre/Viable_titre_psGUI_V4_12-08.py
@@ -37,23 +37,23 @@ header_list = []
 layout1 = [
     [sg.Text('Calculate the Viable Titre of your Strain:', font=font)],
     [sg.Text('Strain', size=(10, 1), font=font),
-        sg.InputText(key='Strain', size=(15, 1), font=font, disabled=False), sg.Checkbox('', default=1, key='1_check')],
+        sg.InputText(key='Strain', size=(15, 1), font=font, disabled=False), sg.Checkbox('', default=1, key='1_check', change_submits = True, enable_events=True)],
     [sg.Text('Condition', size=(10, 1), font=font),
-        sg.Combo(['LB', 'Kan', 'Cm', 'Tc', 'Rif'], key='Condition', size=(15, 1), font=font, disabled=False), sg.Checkbox('', default=1, key='2_check')],
+        sg.Combo(['LB', 'Kan', 'Cm', 'Tc', 'Rif'], key='Condition', size=(15, 1), font=font, disabled=False), sg.Checkbox('', default=1, key='2_check', change_submits = True, enable_events=True)],
     [sg.Text('Time (mins)', size=(10, 1), font=font),
         sg.Combo(['0', '30', '60', '90', '120', '150', '180'], key='Time', size=(15, 1), font=font, disabled=False), sg.Checkbox('', default=1, key='3_check', change_submits = True, enable_events=True)],
     [sg.Text('Volume (mL)', size=(10, 1), font=font),
-        sg.Combo(['0.1', '0.01'], key='Volume', size=(15, 1), font=font, disabled=False), sg.Checkbox('', default=1, key='4_check')],
+        sg.Combo(['0.1', '0.01'], key='Volume', size=(15, 1), font=font, disabled=False), sg.Checkbox('', default=1, key='4_check', change_submits = True, enable_events=True)],
     [sg.Text('Dilution_1',  size=(10, 1), font=font),
-        sg.Combo(['10e-1', '10e-2', '10e-3', '10e-4', '10e-5', '10e-6', '10e-7', '10e-8'], key='Dilution_1', size=(15, 1), font=font, disabled=False), sg.Checkbox('', default=1, key='5_check')],
+        sg.Combo(['10e-1', '10e-2', '10e-3', '10e-4', '10e-5', '10e-6', '10e-7', '10e-8'], key='Dilution_1', size=(15, 1), font=font, disabled=False), sg.Checkbox('', default=1, key='5_check', change_submits = True, enable_events=True)],
     [sg.Text('Dilution_2',  size=(10, 1), font=font),
-        sg.Combo(['10e-1', '10e-2', '10e-3', '10e-4', '10e-5', '10e-6', '10e-7', '10e-8'], key='Dilution_2', size=(15, 1), font=font, disabled=False), sg.Checkbox('', default=1, key='6_check')],
+        sg.Combo(['10e-1', '10e-2', '10e-3', '10e-4', '10e-5', '10e-6', '10e-7', '10e-8'], key='Dilution_2', size=(15, 1), font=font, disabled=False), sg.Checkbox('', default=1, key='6_check', change_submits = True, enable_events=True)],
     [sg.Text('Colonies_1', size=(10, 1), font=font), sg.InputText(key='Colonies_1A', size=(15, 1), font=font),
-        sg.InputText(key='Colonies_1B', size=(15, 1), font=font, disabled=False), sg.Checkbox('', default=1, key='7_check')],
+        sg.InputText(key='Colonies_1B', size=(15, 1), font=font, disabled=False), sg.Checkbox('', default=1, key='7_check', change_submits = True, enable_events=True)],
     [sg.Text('Colonies_2', size=(10, 1), font=font), sg.InputText(key='Colonies_2A', size=(15, 1), font=font),
-        sg.InputText(key='Colonies_2B', size=(15, 1), font=font, disabled=False), sg.Checkbox('', default=1, key='8_check')],
+        sg.InputText(key='Colonies_2B', size=(15, 1), font=font, disabled=False), sg.Checkbox('', default=1, key='8_check', change_submits = True, enable_events=True)],
     [sg.Text('Viable Titre', size=(10, 1), font=font),
-        sg.InputText('', key='Titre', size=(15, 1), font=font, disabled=False), sg.Checkbox('', default=1, key='9_check')],
+        sg.InputText('', key='Titre', size=(15, 1), font=font, disabled=False), sg.Checkbox('', default=1, key='9_check', change_submits = True, enable_events=True)],
     [sg.Text("Choose a spreadsheet to update: "), sg.FileBrowse(key='-FILE-')],
     [sg.Button('Calculate cells/mL', font=font), sg.Submit('Update Spreadsheet', font=font), sg.Button('Clear', font=font), sg.Exit(font=font)],
     [sg.Button('View Data', font=font)],
@@ -64,6 +64,9 @@ layout1 = [
 # set up the window and the layout to use for the window
 window = sg.Window('Viable Titre GUI', layout1) #size=(600, 450)
 
+# empty list to append checked keys into
+checked = []
+
 # while loop to keep the window up unless user closes it
 while True:
     event, values = window.read()
@@ -71,10 +74,55 @@ while True:
         break
     if event == 'Clear':
         clear_input()
-    if values['3_check'] == False:
+    if values['1_check'] == 0:
+        window['Strain'].Update(disabled = True)
+    if values['1_check'] == 1:
+        window['Strain'].Update(disabled = False)
+        try:
+            checked.append(['Strain']) # need a way to not duplicate the key LO 16/08
+            print(checked)
+        except:
+            print('NOPE')
+    if values['2_check'] == 0:
+        window['Condition'].Update(disabled = True)
+    if values['2_check'] == 1:
+        window['Condition'].Update(disabled = False)
+    if values['3_check'] == 0:
         window['Time'].Update(disabled = True)
-    if values['3_check'] == True:
+    if values['3_check'] == 1:
         window['Time'].Update(disabled = False)
+    if values['4_check'] == 0:
+        window['Volume'].Update(disabled = True)
+    if values['4_check'] == 1:
+        window['Volume'].Update(disabled = False)
+    if values['5_check'] == 0:
+        window['Dilution_1'].Update(disabled = True)
+    if values['5_check'] == 1:
+        window['Dilution_1'].Update(disabled = False)
+    if values['6_check'] == 0:
+        window['Dilution_2'].Update(disabled = True)
+    if values['6_check'] == 1:
+        window['Dilution_2'].Update(disabled = False)
+    if values['7_check'] == 0:
+        window['Colonies_1A'].Update(disabled = True)
+    if values['7_check'] == 1:
+        window['Colonies_1A'].Update(disabled = False)
+    if values['7_check'] == 0:
+        window['Colonies_1B'].Update(disabled = True)
+    if values['7_check'] == 1:
+        window['Colonies_1B'].Update(disabled = False)
+    if values['8_check'] == 0:
+        window['Colonies_2A'].Update(disabled = True)
+    if values['8_check'] == 1:
+        window['Colonies_2A'].Update(disabled = False)
+    if values['8_check'] == 0:
+        window['Colonies_2B'].Update(disabled = True)
+    if values['8_check'] == 1:
+        window['Colonies_2B'].Update(disabled = False)
+    if values['9_check'] == 0:
+        window['Titre'].Update(disabled = True)
+    if values['9_check'] == 1:
+        window['Titre'].Update(disabled = False)
     if event == 'Calculate cells/mL':
         average1 = np.mean([int(values['Colonies_1A']), int(values['Colonies_1B'])])
         average2 = np.mean([int(values['Colonies_2A']), int(values['Colonies_2B'])])
@@ -86,12 +134,8 @@ while True:
         EXCEL_FILE = values['-FILE-']
         df = pd.read_excel(EXCEL_FILE)
         df = df.append(values, ignore_index=True)
+        #df = df.append(values, ignore_index=True)
         # Trying to only keep columns which are checked
-        '''
-        for key in values:
-            window[key]
-            if key ==  True:
-                df = df.append(values, ignore_index=True)'''
         #df = df.drop(['-FILE-'], axis=1)
         df = df.drop(['-FILE-', '1_check', '2_check', '3_check', '4_check', '5_check', '6_check', '7_check', '8_check', '9_check'], axis='columns')
         df.to_excel(EXCEL_FILE, index=False)
@@ -111,10 +155,11 @@ while True:
             layout_data = [
                 [sg.Table(values=data, headings=header_list, display_row_numbers=False, auto_size_columns=False, num_rows=min(25, len(data)), alternating_row_color='teal')]
             ]
-            window_data = sg.Window('Table', layout_data)
+            window_data = sg.Window('output_'+day+'-'+month+'.csv', layout_data)
             event, values = window_data.read()
         except:
             sg.popup_error('YOU DIED')
+            window.close()
     if event == 'Plot Data':
         sg.popup('Feature not ready')
         '''
