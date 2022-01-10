14af57e5a7d30de4dc0ec7c2d425b6b9b35324f7
Dan Goodall <59060337+Armbarbarian@users.noreply.github.com>
2022-01-10 12:38:06 +0000

    kite addition

---
+++ Genomics/test_setup.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+my_dict = {'A': (1, 2, 3), 'B': (2, 3, 4), 'C': (5, 6, 7)}
+
+my_df = pd.DataFrame(my_dict)
+
+my_df
+
+dict2 = {'A': (45, 54, 45), 'B': (5, 6, 6), 'C': (3, 3, 3)}
+
+df2 = pd.DataFrame(dict2)
+
+df2
