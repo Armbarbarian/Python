e905d6edd944d03c2c789fe4d6513f551b22f56c
Dan Goodall <daniel_goodall@outlook.com>
2022-01-14 20:28:15 +0000

    Fork trap app Update
    Trying to add the BLAST App into this App to add OtherFeature from the BLASTcsv
    Need to tidy up the classes and call them later in the GUI efficiently.

--- Genomics/Fork_Trap_App/Fork_Trap_App_V1_11-01.py
+++ Genomics/Fork_Trap_App/Fork_Trap_App_V1_11-01.py
@@ -22,8 +22,13 @@ Great tutorial on how to get started here:
 https://biopython-tutorial.readthedocs.io/en/latest/notebooks/17%20-%20Graphics%20including%20GenomeDiagram.html
 '''
 
-#________________________________________________________________________
+# ________________________________________________________________________
 # Back end code
+
+####################
+#    Ter classes
+####################
+
 
 class TerSite():
     def __init__(self, name, csv):
@@ -39,25 +44,61 @@ class TerSite():
             self.strand = -1
 
 # set up my own class to make it eaaier to add features to set
-class FeatureToSet():
+
+
+class TerToSet():
     def __init__(self, set, ter_site, colour):
         self.set = set
         self.ter_site = ter_site
         self.colour = colour
-        feature = SeqFeature(FeatureLocation(ter_site.start, ter_site.stop), strand=ter_site.strand)
-        set.add_feature(feature, name=ter_site.name, label=True, label_size=25, label_position="end", sigil="ARROW", color=colour, arrowhead_length=1000)
+        feature = SeqFeature(FeatureLocation(
+            ter_site.start, ter_site.stop), strand=ter_site.strand)
+        set.add_feature(feature, name=ter_site.name, label=True, label_size=25,
+                        label_position="end", sigil="ARROW", color=colour, arrowhead_length=1000)
+
+
+####################
+#    BLAST classes
+####################
+
+class BlastSite():
+    def __init__(self, name, csv):
+        self.name = name
+        self.start = int(csv[csv.qname == name].pos)
+        self.stop = int(csv[csv.qname == name].pos)+20000
+        self.strand_pos = csv[csv.qname == name].strand.tolist()[0]
+        self.seq = csv[csv.qname == name].seq
+        self.csv = csv[csv.qname == name]
+        if self.strand_pos == '-':
+            self.strand = +1
+        if self.strand_pos == '+':
+            self.strand = -1
+
+
+class BlastToSet():
+    def __init__(self, set, site, colour):
+        self.set = set
+        self.sit = site
+        self.colour = colour
+        feature = SeqFeature(FeatureLocation(
+            site.start, site.stop), strand=site.strand)
+        set.add_feature(feature, name=site.name, label=True, label_size=25,
+                        label_position="end", sigil="ARROW", color=colour, arrowhead_length=1000)
+
 
 # ________________________________________________________________________
 # GUI
-#________________________________________________________________________
-sg.theme('DarkGrey') # DarkGrey14, DarkGrey, Reddit, LightBlue, DarkBlue1
+# ________________________________________________________________________
+sg.theme('DarkGrey')  # DarkGrey14, DarkGrey, Reddit, LightBlue, DarkBlue1
 font = ('Calibri', 14)
+font_heading = ('Calibri', 24)
 font_small = ('Calibri', 12)
 
 layout_text = [
+    [sg.Text('Fork Trap App', font=font_heading)],
     [sg.Text('Select Genome (fasta): ', font=font)],
-    [sg.Text('Select BowTie2 csv: ', font=font)],
-    #[sg.Text('Select ter site: ', font=font)],
+    [sg.Text('Ter BT2 csv: ', font=font)],
+    [sg.Text('BLAST csv: ', font=font)],
     [sg.Text('Chromosome Shape: ', font=font)],
     [sg.Text(' '*20)],
     [sg.Text('Save file as: ', font=font)],
@@ -65,17 +106,22 @@ layout_text = [
 ]
 
 layout_input = [
-    [sg.FileBrowse(key = '-genome1-')],
-    [sg.FileBrowse(key = '-BT2_csv-')],
-    #[sg.Combo(['All', 'terA', 'terB', 'terC', 'terD', 'terE', 'terF', 'terG', 'terH', 'terI', 'terJ'],key = '-ter_input-')],
-    [sg.Combo(['linear', 'cicular'], key = '-chromosome_shape-', font=font)],
+    [sg.Text(' '*20, font=font_heading)],
+    [sg.FileBrowse(key='-genome1-')],
+    [sg.FileBrowse(key='-BT2_csv-')],
+    [sg.FileBrowse(key='-BLAST_csv-')],
+    # [sg.Combo(['All', 'terA', 'terB', 'terC', 'terD', 'terE', 'terF', 'terG', 'terH', 'terI', 'terJ'],key = '-ter_input-')],
+    [sg.Combo(['linear', 'cicular'], key='-chromosome_shape-', font=font)],
     [sg.Text(' '*20)],
-    [sg.InputText(key = '-image_name-', font=font)],
-    [sg.FileBrowse(key = '-save_image-')],
+    [sg.InputText(key='-image_name-', font=font)],
+    [sg.FileBrowse(key='-save_image-')],
 ]
 
+# layout pad pad=((0, 0), (40, 0)))
+
 layout_cols = [
-    [sg.Column(layout_text, pad=((0, 0), (40, 0))), sg.Column(layout_input, pad=((0, 0), (40, 0)))], [sg.Button('Run', font=font)]
+    [sg.Column(layout_text), sg.Column(layout_input)],
+    [sg.Button('Run', font=font)]
 ]
 
 window1 = sg.Window('Fork Trap App', layout_cols, resizable=True)
@@ -87,9 +133,12 @@ while True:
         try:
             fas = list(SeqIO.parse(open(values['-genome1-']), 'fasta'))
             sg.popup('Genome size: ' + str(len(fas[0])))
-            csv = pd.read_csv(values['-BT2_csv-'])
-            sg.popup(csv)
-            # create TerSites
+            ter_csv = pd.read_csv(values['-BT2_csv-'])
+            BLAST_csv = pd.read_csv(values['-BLAST_csv-'])
+            # sg.popup(csv
+
+            # create Sites
+            # Ter specific sites
             terA = TerSite('terA', csv)
             terB = TerSite('terB', csv)
             terC = TerSite('terC', csv)
@@ -100,6 +149,9 @@ while True:
             terH = TerSite('terH', csv)
             terI = TerSite('terI', csv)
             terJ = TerSite('terJ', csv)
+
+            # BLAST sites
+
             # ___________________
             # Genome Diagram using TerSite class
             # ___________________
@@ -124,23 +176,14 @@ while True:
         os.chdir(dir_path)
 
         # Add the terA TerSite class information into the Track
-        gd_diagram.draw(format=values['-chromosome_shape-'], start=0, end=len(fas[0]), circle_core = 0.8)
+        gd_diagram.draw(
+            format=values['-chromosome_shape-'], start=0, end=len(fas[0]), circle_core=0.8)
         if not values['-image_name-']:
             gd_diagram.write('output_'+day+'-'+month+'.pdf', 'pdf')
             gd_diagram.write('output_'+day+'-'+month+'.png', 'png')
         else:
             gd_diagram.write(values['-image_name-']+'.pdf', 'pdf')
             gd_diagram.write(values['-image_name-']+'.png', 'png')
-
-
-
-
-
-
-
-
-
-
 
 
 # fragments=3, pagesize=(15*cm, 4*cm)
